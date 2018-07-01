0e9dbc4597fb7b554779fff1afe736d03b6161d7
Gorilla12321 <39292762+Gorilla12321@users.noreply.github.com>
2018-07-02 03:14:23 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,18 @@
 public class HelloWorld{
+  //comment: This is a test for github 
   public static void main(String args[]){
     System.out.println("Hello World");
+  }
+  //Here are some useful method
+  public void bubbleSort(int [] array){
+    for(int i=0; i<array.length-1;i++){
+      for(int j=0;j<array.length-1;j++){
+        if(array[j]>array[j+1]){
+          int temp = array[j+1];
+          array[j+1]=array[j];
+          array[j]=temp;
+        }
+      }    
+    }
   }
 }
